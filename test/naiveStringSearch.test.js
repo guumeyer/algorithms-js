d58e44ce31eb7d2aa331ed8a601959583e7011f9
gmeyer <guumeyer@gmail.com>
2021-10-10 23:33:19 -0700

    Add naive string search frequency

--- test/naiveStringSearch.test.js
+++ test/naiveStringSearch.test.js
@@ -1,7 +1,13 @@
 const { naiveSearchString } = require('../src/naiveSearchString');
 
-test('should find 2 occurence of lon into lonborlon', function() {
+test('should found 2 occurence of lon into lonborlon', function() {
     const result = naiveSearchString('lonborlon', 'lon');
 
     expect(result).toBe(2);
+});
+
+test('should not found of abc into abdacf', function() {
+    const result = naiveSearchString('abdacf', 'abc');
+
+    expect(result).toBe(0);
 });

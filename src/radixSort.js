e1587d3a7c84b76094a0fc04efe02a72e29f0aac
gmeyer <guumeyer@gmail.com>
2021-10-13 15:12:17 -0700

    clean get digit

--- src/radixSort.js
+++ src/radixSort.js
@@ -1,7 +1,5 @@
 function getDigit(number, position) {
-
     return Math.floor(Math.abs(number) / Math.pow(10, position)) %10;
-    // return Math.floor(( Math.abs(number) % Math.pow(10, (position + 1))) / Math.pow(10, position));
 }
 
 

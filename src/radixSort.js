51523e29c5bbb8cd7fd49494cc6db83e67690e72
gmeyer <guumeyer@gmail.com>
2021-10-13 15:18:12 -0700

    get number of digits

--- src/radixSort.js
+++ src/radixSort.js
@@ -2,7 +2,12 @@ function getDigit(number, position) {
     return Math.floor(Math.abs(number) / Math.pow(10, position)) %10;
 }
 
+function getNumberOfDigets(number) {
+    return Math.floor(Math.log10(Math.abs(number))) + 1;
+}
+
 
 module.exports = {
-    getDigit
+    getDigit,
+    getNumberOfDigets
 }
--- test/radixSort.test.js
+++ test/radixSort.test.js
@@ -1,4 +1,4 @@
-const { getDigit } = require('../src/radixSort');
+const { getDigit, getNumberOfDigets } = require('../src/radixSort');
 
 
 test('should get 1st number of 12345 and return 5', function () {
@@ -11,4 +11,16 @@ test('should get 1st number from negative -12345 and return 5', function () {
     const result  = getDigit(-12345, 0); 
 
     expect(result).toBe(5);
+})
+
+test('should return number 4 digets from number 1235 ', function() {
+    const result = getNumberOfDigets(1235);
+
+    expect(result).toBe(4);
+})
+
+test('should return number 3 digets from negative number 123 ', function() {
+    const result = getNumberOfDigets(-123);
+
+    expect(result).toBe(3);
 })

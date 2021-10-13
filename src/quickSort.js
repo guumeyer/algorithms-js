4f9240a7c168c36da957df0c1eee11ccfc05ab5d
gmeyer <guumeyer@gmail.com>
2021-10-13 09:44:29 -0700

    QRAdd quicksort

--- src/quickSort.js
+++ src/quickSort.js
@@ -1,9 +1,13 @@
+// time: avg O(n log n), worst: O(n^2)
+// space: O(1), but the call stack will be O(log n) because of the recursion
+function quickSort(array) {
+    _quickSort(array, 0, (array.length - 1));
 
-// time: avg O(n log n), worst: O(n^2), space: O(n) 
-function _quickSourt(array, startIdx, endIdx) {
-    if(startIdx >= endIdx) {
-        return;
-    }
+    return array;
+}
+
+function _quickSort(array, startIdx, endIdx) {
+    if (startIdx >= endIdx) return;
 
     const pivotIdx = startIdx;
     let leftIdx = startIdx + 1;
@@ -13,29 +17,19 @@ function _quickSourt(array, startIdx, endIdx) {
         if (array[leftIdx] > array[pivotIdx] && array[rightIdx] < array[pivotIdx]) {
             swap(leftIdx, rightIdx, array);
         }
-        if (array[leftIdx] <= array[pivotIdx]) {
-            leftIdx ++;
-        }
-        if (array[rightIdx] >= array[pivotIdx]) {
-            rightIdx --;
-        }
+
+        if (array[leftIdx] <= array[pivotIdx]) leftIdx ++;
+        if (array[rightIdx] >= array[pivotIdx]) rightIdx --;
     }
 
-    swap(rightIdx, pivotIdx, array);
+    swap(pivotIdx, rightIdx, array);
 
-    _quickSourt(array, (rightIdx + 1), endIdx);
-    _quickSourt(array, startIdx, (rightIdx - 1));
+    _quickSort(array, (rightIdx + 1), endIdx);
+    _quickSort(array, startIdx, (rightIdx - 1));
 }
 
 function swap(i, j, array) {
-    const temp = array[i];
-    array[i] = array[j];
-    array[j] = temp;
-}
-
-function quickSort(array) {
-    _quickSourt(array, 0, array.length - 1);
-    return array;
+    [array[j], array[i]] = [array[i], array[j]];
 }
 
 module.exports = {
